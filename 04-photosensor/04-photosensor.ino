2694e3cdd31086f33a7e2de2eea15a6e5ee4b099
Holly Hudson <holly@local>
2018-07-28 18:17:27 +0200

    adds code to play final countdown on a buzzer

--- 04-photosensor/04-photosensor.ino
+++ 04-photosensor/04-photosensor.ino
@@ -1,18 +1,30 @@
-/* Things to try:
+/* 
+ * tone(pin, frequency_in_Hz)
  * 
- * 
- * 
+ * Things to try:
+ *
+ * - play a scale using the tones[] array
+ * - play a simple tune
  */
 
-const int PHOTOPIN = 12;
+const int PHOTOPIN = A1;
+const int BUZZPIN = 11;
+int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+//            mid C  C#   D    D#   E    F    F#   G    G#   A
+// 				every octave doubles the frequency
 
 void setup()
 {
 	pinMode(PHOTOPIN, INPUT);
+	pinMode(BUZZPIN, OUTPUT);
+	Serial.begin(9600);
 }
 
 void loop()
 {
-	println(analogRead(PHOTOPIN));
-	delay(100);
+	Serial.println(analogRead(PHOTOPIN));
+	// values are 430 - 780
+	// desired is 200 - 370 Hz, so divide by 2
+	
+	tone(BUZZPIN, (analogRead(PHOTOPIN)/2));	
 }
---
+++ tunes/tunes.ino
@@ -0,0 +1,79 @@
+/* 
+ * tone(pin, frequency_in_Hz)
+ * 
+ * Things to try:
+ *
+ * - play a scale using the tones[] array
+ * - play a simple tune
+ */
+
+const int BUZZPIN = 11;
+const int BEAT = 100;
+int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+//            mid C  C#   D    D#   E    F    F#   G    G#   A
+// 				every octave doubles the frequency
+
+void setup()
+{
+	pinMode(BUZZPIN, OUTPUT);
+}
+
+void loop()
+{
+	tone(BUZZPIN, 659);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 587);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 659);
+	delay(BEAT * 4);
+	tone(BUZZPIN, 440);
+	delay(BEAT * 4);
+	// rest
+	noTone(BUZZPIN);
+	delay(BEAT * 6);
+	// rest
+	tone(BUZZPIN, 698);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 659);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 698);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 659);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 587);
+	delay(BEAT * 4);
+	// rest
+	noTone(BUZZPIN);
+	delay(BEAT * 6);
+	// rest
+	tone(BUZZPIN, 659);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 587);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 659);
+	delay(BEAT * 4);
+	tone(BUZZPIN, 440);
+	delay(BEAT * 4);
+	// rest
+	noTone(BUZZPIN);
+	delay(BEAT * 6);
+	// rest
+	tone(BUZZPIN, 587);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 523);
+	delay(BEAT * 1);
+	tone(BUZZPIN, 587);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 523);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 494);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 587);
+	delay(BEAT * 2);
+	tone(BUZZPIN, 523);
+	delay(BEAT * 6);
+	// rest
+	noTone(BUZZPIN);
+	delay(BEAT * 6);
+	// rest
+}
